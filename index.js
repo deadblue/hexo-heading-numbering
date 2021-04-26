4386b0a830d6fb8663b5cbc12419330739b56e42
Tomo Kunagisa <root@dead.blue>
2021-04-26 22:39:28 +0800

    Change layout before processing.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const logger = hexo.log || console;
 const hn = require('./lib/heading-numbering');
 
 hexo.extend.filter.register('before_post_render', data => {
+    // Skip non-post pages.
+    if ('post' !== data['layout']) {
+        return;
+    }
     const config = hexo.config['heading_numbering'];
     if ('undefined' !== config && config['enable'] === true) {
         logger.log(`Numbering heading for: ${data.path}`);
